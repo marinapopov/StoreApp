8437a264cc3573372a4e7b672b16ada6d09570c2
Marina <popovmarina8@gmail.com>
2022-03-16 10:32:12 +0200

    Initial commit Create Welcome page and Login Activity

---
+++ app/src/main/java/com/barbaracollection/barbaracollection/MainActivity.java
@@ -0,0 +1,34 @@
+package com.barbaracollection.barbaracollection;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.barbaracollection.barbaracollection.ui.login.LoginActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
